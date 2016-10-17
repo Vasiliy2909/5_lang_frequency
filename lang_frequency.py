ee53b7b7eabf5cb11bfb5c4a84fa290c0434c91c
Vasiliy2909 <vasiliyzh2909@yndex.ru>
2016-10-18 01:25:28 +0300

    Вывод частоты слов

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,27 @@
+import os
+import re
+import collections
+
+
+def input_way_to_filepath():
+    way = input('Укажите путь к файлу:')
+    if os.path.exists(way):
+        return way
 
 
 def load_data(filepath):
-    pass
+    if os.path.exists(filepath):
+        with open(filepath, 'r') as words:
+            return words.read()
 
 
 def get_most_frequent_words(text):
-    pass
+    words_text = re.sub(r'[^\w]', ' ', text).lower().split()
+    repeated_words = collections.Counter(words_text).most_common(10)
+    return repeated_words
 
 
 if __name__ == '__main__':
-    pass
+    filepath = input_way_to_filepath()
+    text = load_data(filepath)
+    print(get_most_frequent_words(text))
